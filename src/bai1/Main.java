cafbb4ae5a9a19e31c438597b2e9da3c9df337d2
admin <admin@admins-MacBook-Air.local>
2024-05-22 08:05:03 +0700

    start

---
+++ src/bai1/Main.java
@@ -0,0 +1,15 @@
+package bai1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a, b;
+        System.out.println("Nhập a, b: ");
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+
+
+    }
+}
